61f2a3c79b2f453e1cfdf07c91f3dadb2d70325a
willhurleyy <106354843+willhurleyy@users.noreply.github.com>
2023-12-11 19:46:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from tkinter import *
 import random
 from PIL import ImageTk, Image
 import requests
-from io import BytesIO
 root = Tk()
 root.title("poker")
-root.geometry("800x600")
+root.geometry("1920x1080")
 root.configure(bg="green")
 
 my_frame = Frame(root, bg="green")
@@ -32,11 +31,15 @@ def shuffle():
     player2.append(random.choice(deck))
     deck.remove(player2[-1])
 
-    global player1_image, player2_image
-    player1_image = resize_card(Image.open("C:/Users/willi/Downloads/PNG-cards-1.3/" + player1[-1] + ".png"))
-    player2_image = resize_card(Image.open("C:/Users/willi/Downloads/PNG-cards-1.3/" + player2[-1] + ".png"))
-    player1_label.config(image = player1_image)
-    player2_label.config(image = player2_image)
+    global player1card1_image, player1card2_image, player2card1_image, player2card2_image
+    player1card1_image = resize_card(Image.open("C:/Users/willi/Downloads/PNG-cards-1.3/" + player1[-1] + ".png"))
+    player1card2_image = resize_card(Image.open("C:/Users/willi/Downloads/PNG-cards-1.3/" + player1[-2] + ".png"))
+    player2card1_image = resize_card(Image.open("C:/Users/willi/Downloads/PNG-cards-1.3/" + player2[-1] + ".png"))
+    player2card2_image = resize_card(Image.open("C:/Users/willi/Downloads/PNG-cards-1.3/" + player2[-2] + ".png"))
+    player1card1_label.config(image = player1_image)
+    player1card2_label.config(image = player1_image)
+    player2card1_label.config(image = player2_image)
+    player2card2_label.config(image = player2_image)
 
 def deal():
     player1.append(random.choice(deck))
@@ -51,23 +54,50 @@ def deal():
     player1_label.config(image = player1_image)
     player2_label.config(image = player2_image)
 
+def facedown():
+    global player1_image, player2_image
+    player1_image = resize_card(Image.open("C:/Users/willi/Downloads/PNG-cards-1.3/card-face-down.png"))
+    player2_image = resize_card(Image.open("C:/Users/willi/Downloads/PNG-cards-1.3/card-face-down.png"))
+    player1_label.config(image = player1_image)
+    player2_label.config(image = player2_image)
 
-player1_frame = LabelFrame(my_frame, text = "Player 1", bd = 0)
-player1_frame.grid(row=0, column=0, padx=10)
-player2_frame = LabelFrame(my_frame, text = "Player 2", bd = 0)
-player2_frame.grid(row=0, column=1, padx=10)
+def faceup():
+    global player1_image, player2_image
+    player1_image = resize_card(Image.open("C:/Users/willi/Downloads/PNG-cards-1.3/" + player1[-1] + ".png"))
+    player2_image = resize_card(Image.open("C:/Users/willi/Downloads/PNG-cards-1.3/" + player2[-1] + ".png"))
+    player1card1_label.config(image = player1_image)
+    player2card1_label.config(image = player2_image)
 
-player1_label = Label(player1_frame, text = "")
-player1_label.grid(row=0, column=0, padx=10)
-player2_label = Label(player2_frame, text = "")
-player2_label.grid(row=0, column=0, padx=10)
+
+player1card1_frame = LabelFrame(my_frame, text = "Player 1", bd = 0)
+player1card1_frame.grid(row=0, column=0, pady=100)
+player1card2_frame = LabelFrame(my_frame, text = "Player 1", bd = 0)
+player1card2_frame.grid(row=0, column=1, pady=100)
+player2card1_frame = LabelFrame(my_frame, text = "Player 2", bd = 0)
+player2card1_frame.grid(row=0, column=1, pady=100)
+player2card2_frame = LabelFrame(my_frame, text = "Player 2", bd = 0)
+player2card2_frame.grid(row=0, column=2, pady=100)
+
+player1card1_label = Label(player1card1_frame, text = "")
+player1card1_label.grid(row=0, column=0, padx=10)
+player1card2_label = Label(player1card2_frame, text = "")
+player1card2_label.grid(row=0, column=0, padx=10)
+player2card1_label = Label(player2card1_frame, text = "")
+player2card1_label.grid(row=0, column=0, padx=10)
+player2card2_label = Label(player2card2_frame, text = "")
+player2card2_label.grid(row=0, column=0, padx=10)
 
 shuffle_button = Button(root, text = "Shuffle", command = shuffle)
-shuffle_button.pack(pady=10)
+shuffle_button.pack(pady=0)
 
 deal_button = Button(root, text = "Deal", command = deal)
-deal_button.pack(pady=10)
+deal_button.pack(pady=0)
 
+facedown_button = Button(root, text = "Facedown", command = facedown)
+facedown_button.pack(pady=0)
+
+faceup_button = Button(root, text = "Faceup", command = faceup)
+faceup_button.pack(pady=0)
 shuffle()
 
 
